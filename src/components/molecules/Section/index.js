597b409919e8ac969bb141bec16b60472d4654de
maliksaepul <malik@inspigo.id>
2021-08-18 11:58:13 +0700

    bismillah mandiri learning

--- src/components/molecules/Section/index.js
+++ src/components/molecules/Section/index.js
@@ -1,24 +1,29 @@
-import React from 'react';
-import * as styles from './section.module.scss'
-import cx from 'classnames';
+import React from "react"
+import * as styles from "./section.module.scss"
+import cx from "classnames"
 
-
-const Section = (props) => {
-    const {children, className, id} = props;
-    return <section className={cx(styles.section)} id={id} style={props.style || {}}>
-        {
-            props.decoration && props.decoration.length > 0 ? 
-            props.decoration.map((val, key) => {
-                return <div className={styles.section_decoration} style={val.style} key={key}>
-                    <img src={val.src} alt="decoration" />
-                </div>
-            })
-            : null
-        }
-        <div className={cx(styles.section_container, className )}>
-        {children}
-        </div>
-        </section>
+const Section = props => {
+  const { children, className, id } = props
+  return (
+    <section className={cx(styles.section)} id={id} style={props.style || {}}>
+      {props.decoration && props.decoration.length > 0
+        ? props.decoration.map((val, key) => {
+            return (
+              <div
+                style={val.style}
+                key={key}
+              >
+                <img
+                  src={val.url}
+                  alt="decoration"
+                />
+              </div>
+            )
+          })
+        : null}
+      <div className={cx(styles.section_container, className)}>{children}</div>
+    </section>
+  )
 }
 
-export default Section;
+export default Section
