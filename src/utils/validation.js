5533cc86f9fba5779f8cdb4b6538cb188e5472ee
maliksaepul <malik@inspigo.id>
2021-08-23 14:42:44 +0700

    validation

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -1,4 +1,4 @@
-export const isValidNIPMandiri =  string => string && !isNaN(string);
+export const isValidNIPMandiri =  string => string && !isNaN(string) && string.length == 10;
 export  const validateEmail = email => {
     const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(email);
